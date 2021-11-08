593e39ab1e07f091ae177fc46586133525f3f3d6
mohammedismayil <mohammedismayil10@gmail.com>
2021-11-09 00:37:31 +0530

    fixed path issue in localhost -- need to setup in heroku

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 // server.js
 const express = require('express');
 const app = express();
-
+const path = require('path');
 const routes = require('./routes');
 
 const PORT = process.env.PORT || 5000;
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // ADD THIS LINE
-app.use(express.static('client/build'));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.use(routes);
 
